4cbffb6b261e233aaa3a4e13aabbe0fc7a880184
Muhammadizhan <izhanayaz4@gmail.com>
2024-04-24 00:47:08 +0500

    modified JS file

--- script.js
+++ script.js
@@ -10,29 +10,31 @@ function tossCoin(choice) {
   var resultText = document.getElementById("result");
   var coin = document.getElementById("coin");
 
-  // Disable buttons during animation
+  
   document.getElementById("headsButton").disabled = true;
   document.getElementById("tailsButton").disabled = true;
 
-  // Show the coin
+
   coin.style.display = "block";
 
-  // Clear previous result
+
   resultText.innerText = "";
 
-  // Simulate coin toss
+ 
   setTimeout(function () {
     // Enable buttons after animation finishes
     document.getElementById("headsButton").disabled = false;
     document.getElementById("tailsButton").disabled = false;
 
-    // Hide the coin
+   
     coin.style.display = "none";
 
-    // Randomly determine result
-    var result = Math.floor(Math.random() * 2) + 1; // Use 1 for Heads and 2 for Tails
+  
+    var result = Math.floor(Math.random() * 2) + 1;
 
-    // Show result message
+
+
+    
     if (
       (result === 1 && choice === "Heads") ||
       (result === 2 && choice === "Tails")
@@ -41,5 +43,5 @@ function tossCoin(choice) {
     } else {
       resultText.innerText = "Sorry! You lose!";
     }
-  }, 1000); // Adjust timing to match the toss animation duration
+  }, 1000);
 }
